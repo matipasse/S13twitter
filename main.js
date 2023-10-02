511f25a2e2330ef224708e6237d7ca6c2a5b3533
matipasse <matiaspasserini51@gmail.com>
2023-10-01 22:35:18 -0300

    Terminado

--- main.js
+++ main.js
@@ -12,25 +12,25 @@ function comentarios(data) {
 
   for (let i = 0; i < data.length; i++) {
     htmlContentToAppend += `
-    <div class="profile-pic"><img src="https://xsgames.co/randomusers/avatar.php?g=pixel " /></div>
-          <div class="content">
-            <div class="names">
-              <p class="full-name">${data[i].name}</p>
-              <p class="user-name">${data[i].username}</p>
-              <p class="time"> ${data[i].time}</p>
-            </div>
-          </div>
-          <div class="tweet-content">
-            <p>${data[i].text}</p>
-          </div>
-          <div class="tweet-icons">
-            <i class="fa fa-comment" aria-hidden="true"></i>
-            <i class="fa fa-heart" aria-hidden="true"></i>
-            <i class="fa fa-retweet" aria-hidden="true"></i>
-          </div>
+    <div class="tweet">
+      <div class="user-info">
+        <div class="profile-pic"><img class="imagen" src="https://xsgames.co/randomusers/assets/avatars/pixel/${[i]}.jpg " /></div>
+        <p class="full-name">${data[i].name} &nbsp;</p>
+        <p class="user-name">${data[i].username} &nbsp; ·</p>   
+        <p class="time"> &nbsp; ${data[i].time}</p> 
+      </div>
+      <div class="tweet-content">
+        <p>${data[i].text}</p>
+      </div>
+      <div class="tweet-icons">
+        <i class="fa fa-comment" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+        <i class="fa fa-retweet" aria-hidden="true"></i>
+      </div>
+    </div>
     `;
-  
-      };
 
-      document.getElementById("tweets").innerHTML = htmlContentToAppend;
-    };
+  };
+
+  document.getElementById("tweets").innerHTML = htmlContentToAppend;
+};
